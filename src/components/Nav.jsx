1ce5b5abd8995f4606a36f1e9a9f0e6cffeea3fd
MENZI-MCHUNU <menzi639@gmail.com>
2025-03-19 12:14:00 +0200

    made a function for the nav bar links so we can reuse it on the hambuger icon later

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import { logo } from "../assets/images";
 import { navigationBarLinks } from "../constants";
 import { hamburger } from "../assets/icons";
+
+function NavBarLinks() {
+  return navigationBarLinks.map((item, index) => (
+    <li key={index}>
+      <a href={item.href} className="leading-normal text-lg text-slate-600">
+        {item.label}
+      </a>
+    </li>
+  ));
+}
+
 function Nav() {
   return (
     <header className="">
@@ -10,16 +21,7 @@ function Nav() {
           <img src={logo} alt="Logo" width={130} height={29} />
         </a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-          {navigationBarLinks.map((item, index) => (
-            <li key={index}>
-              <a
-                href={item.href}
-                className="leading-normal text-lg text-slate-600"
-              >
-                {item.label}
-              </a>
-            </li>
-          ))}
+          <NavBarLinks />
         </ul>
         <div className="hidden max-lg:block">
           <img src={hamburger} alt="Hamburger" width={30} height={30} />
